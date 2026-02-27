21718cd62ee0f943fdbd6589e29f5948f0f9731d
Jason Brooks <cs.jasonbrooks@gmail.com>
2026-02-27 08:53:46 +0100

    Stringify env error

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -17,7 +17,10 @@ const envSchema = z.object({
 const _env = envSchema.safeParse(process.env);
 
 if (!_env.success) {
-  console.error('Invalid environment variables:', z.treeifyError(_env.error));
+  console.error(
+    'Invalid environment variables:',
+    JSON.stringify(z.treeifyError(_env.error)),
+  );
 
   throw new Error('Environment validation failed');
 }
