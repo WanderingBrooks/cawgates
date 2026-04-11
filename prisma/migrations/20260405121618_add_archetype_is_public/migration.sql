2ed45cae9079b6ab60c190d28248d4af9e1f3523
Jason Brooks <cs.jasonbrooks@gmail.com>
2026-04-11 10:58:58 +0200

    document migration

--- prisma/migrations/20260405121618_add_archetype_is_public/migration.sql
+++ prisma/migrations/20260405121618_add_archetype_is_public/migration.sql
@@ -1,2 +1,3 @@
--- AlterTable
-ALTER TABLE "Archetype" ADD COLUMN     "isPublic" BOOLEAN NOT NULL DEFAULT false;
+-- Add column to indicate if an archetype is public or private
+-- Public meaning that other users can view, but not edit it.
+ALTER TABLE "Archetype" ADD COLUMN "isPublic" BOOLEAN NOT NULL DEFAULT false;
