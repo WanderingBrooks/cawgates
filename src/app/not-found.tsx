eebc2410c4aea0f17940b2c1290b0a6a963f20b0
Jason Brooks <cs.jasonbrooks@gmail.com>
2026-04-12 19:41:10 +0200

    Correct not found redirect and add case for non logged in users

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,16 +2,25 @@ import Link from 'next/link';
 import { getTranslations } from 'next-intl/server';
 import classes from './not-found.module.css';
 import { Button } from '@/components';
+import { getUser } from '@/lib/session';
 
 const NotFound = async () => {
   const t = await getTranslations('notFound');
 
+  const user = await getUser();
+
   return (
     <div className={classes.container}>
       <p>{t('message')}</p>
-      <Link href="/">
-        <Button variant="primary">{t('backToHome')}</Button>
-      </Link>
+      {!!user ? (
+        <Link href={`/${user.username}/events`}>
+          <Button variant="primary">{t('goToEvents')}</Button>
+        </Link>
+      ) : (
+        <Link href="/login">
+          <Button variant="primary">{t('goToLogin')}</Button>
+        </Link>
+      )}
     </div>
   );
 };
